5a816e713b677184b9e5cbb3699c4b49ed0108ca
Ru-Pirie <rubens.pirie@gmail.com>
2021-12-23 20:43:31 +0000

    db update

--- src/database/env/script.sql
+++ src/database/env/script.sql
@@ -3,3 +3,4 @@ INSERT INTO SHORTENERS (url, destination, clicks) VALUES ('discord', 'https://di
 INSERT INTO SHORTENERS (url, destination, clicks) VALUES ('twitter', 'https://twitter.com/ru_pirie', 0);
 INSERT INTO SHORTENERS (url, destination, clicks) VALUES ('email', 'mailto:contact@ru-pirie.com', 0);
 INSERT INTO SHORTENERS (url, destination, clicks) VALUES ('wtfarenumbers', 'https://ofqual.blog.gov.uk/2018/03/02/gcse-9-to-1-grades-a-brief-guide-for-parents/', 0);
+INSERT INTO SHORTENERS (url, destination, clicks) VALUES ('gofuckyourself', 'https://pornhub.com', 0);
